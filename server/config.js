c04e01b64d42ef9045362f3d70cf92e67f4825e1
Yahor Litavar <sergioegor21@gmail.com>
2023-02-05 15:49:48 +0200

    fix operation history name

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ export const bankKey = 'Bank1';
 export const commission = 2;
 export const currencyCommission = 3;
 export const USD = 'USD';
-export const OPERATION_HISTORY = 5;
+export const OPERATION_HISTORY_LIMIT = 5;
 export const OPERATION_IDS = {
   'ADD_MONEY': 3,
   'REMOVE_MONEY': 4
