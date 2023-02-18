2ad28a91306cd99a829614c452060013d9dc6c76
Yahor Litavar <sergioegor21@gmail.com>
2023-02-18 12:21:45 +0200

    fix quiz check

--- server/quiz/checkAnswers.js
+++ server/quiz/checkAnswers.js
@@ -2,19 +2,19 @@ import quiz from "../data/quiz.js";
 
 export function checkAnswers(answers) {
   let correct = 0;
-  let answers = [];
+  let corrAnswers = [];
   answers.forEach((el) => {
     const question = quiz.find((q) => q.id == el.id);
     if (el.answer === question.correct.en || el.answer === question.correct.ru) {
       correct++;
     }
-    answers.push({
+    corrAnswers.push({
       en: question.correct.en,
       ru: question.correct.ru
     })
   })
   return {
     correct,
-    answers
+    corrAnswers
   };
 }
